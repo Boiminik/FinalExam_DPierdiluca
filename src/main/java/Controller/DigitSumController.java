c1d644e3d3782453850ccda1c52d61bcc34f1fa5
Dominik Pierdiluca <wi22b124@technikum-wien.at>
2023-06-13 10:18:17 +0200

    added feature/usage + test

--- src/main/java/Controller/DigitSumController.java
+++ src/main/java/Controller/DigitSumController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class DigitSumController {
+    int usgcnt = 0; //the counter for the usages
 
     @RequestMapping ("/api/digitsum") //calculates digitsum of a number
     public int digitSum (@RequestParam int number){
@@ -14,7 +15,12 @@ public class DigitSumController {
             sum = sum + digit; //add it to the digit sum
             number = number/10; //take digit away from number
         }
+        usgcnt++; //adds a count to the usages
         return sum;
+    }
+    @RequestMapping ("/api/usage") //returns how many times the feature digitSum was used
+    public int usage(){
+        return usgcnt;
     }
 
 }
--- src/test/java/Controller/DigitSumControllerTest.java
+++ src/test/java/Controller/DigitSumControllerTest.java
@@ -12,4 +12,11 @@ class DigitSumControllerTest {
         int number = 1234;
         assertEquals(10, dSCont.digitSum(number));
     }
+    @Test
+    void usage(){
+        DigitSumController dSCont = new DigitSumController();
+        dSCont.digitSum(1234);
+        dSCont.digitSum(5678);
+        assertEquals(2, dSCont.usage());
+    }
 }
